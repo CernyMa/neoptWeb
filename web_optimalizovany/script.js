5297c4db3205d1ba211019a4d165f0abcc568d2b
CernyMa <cerny.matej1@zak.spspb.cz>
2025-11-18 10:54:58 +0100

    Delete web_optimalizovany directory

--- web_optimalizovany/script.js
+++
@@ -1,5 +0,0 @@
-
-document.addEventListener('DOMContentLoaded',()=>{
-  const form=document.querySelector('form');
-  if(form){form.addEventListener('submit',e=>{e.preventDefault();alert('Díky! (Simulace)')});}
-});
